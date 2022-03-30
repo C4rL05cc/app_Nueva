35168293e065f350f98be5540350b1a01c31054b
*C4rL05cc* <*carlos.cardoso@udgvirtual.udg.mx*>
2022-03-29 20:17:04 -0600

    actividad 3.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,14 +13,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'LDSW',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+             body: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               crossAxisAlignment: CrossAxisAlignment.center,
+               children: [
+                 Container(
+                   color: Colors.blueAccent,
+                   padding: EdgeInsets.all(30),
+                   child: Row(
+                     children: [
+                       Text('Actividad 3.4', style: TextStyle(
+                         fontSize: 40,
+                         color: Colors.white
+                       )),
+                     ],
+                   ),
+                 ),
+                 Container(
+                   color: Colors.lightBlue,
+                   padding: EdgeInsets.all(15),
+                   child: Row(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                       Text('Utilización de widgets', style: TextStyle(
+                           fontSize: 20,
+                           color: Colors.white
+                       )),
+                     ],
+                   ),
+                 ),
+                 Stack(
+                   alignment: Alignment.topCenter,
+                   children: [
+                     Container(
+                       color: Colors.amber,
+                       child: SizedBox(
+                         height: 150,
+                         width: 150,
+                       ),
+                     ),
+                     Container(
+                       color: Colors.blueGrey,
+                       child: SizedBox(
+                         height: 60,
+                         width: 60,
+                       ),
+                     )
+                   ],
+                 )
+               ],
+             ),
       ),
     );
   }
